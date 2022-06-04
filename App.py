492b916723d963750af915d7ce9c4565808d1011
Ben <feba23@live.com>
2022-06-04 17:02:23 -0500

    Adding documentation and README.md

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template, redirect
+from flask import Flask, jsonify, request, render_template, redirect, send_from_directory
 from flask_cors import CORS
 
 import json
@@ -6,38 +6,72 @@ import json
 from src.routes.sampleExternalRoutes import SampleRoutes
 
 app = Flask(__name__)
+
+# Enables CORS on our application
 CORS(app)
 
+# Example of using an external file for holding routes. URL prefix is optional
 app.register_blueprint(SampleRoutes, url_prefix="/sample")
 
+'''
+Index route
 
+All routes are basic python functions with an @app.route() decorator with a routing name as the parameter
+
+Returning a "render_template" instead of just a file object will allow the browser to display the html file correctly as a file
+'''
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
+'''
+Example Serving static files
+'''
+@app.route('/static/<path:path>')
+def send_report(path):
+    return send_from_directory('static', path)
+
+'''
+Example JSON endpoint
+
+Returning a jsonify function will allow us to transform a dictionary into a json object with all the correct json headers
+'''
 @app.route("/jsonEndpoint/")
 def statusRoute():
     return jsonify({"Sample Json": True})
 
 
+'''
+Example of getting a variable from the url. You can also pass in integers, doubles, or booleans
+'''
 @app.route("/dynamicRouteVariable/<string:name>/")
 def dynamicRouteVariable(name: str):
     return jsonify({"name": name})
 
 
+'''
+Example of getting data from the request object. Aall the incoming request data can be accessed through the request object.
+
+Accessing specific body/cookie data can be gotten through request.form.get("KEY") or request.cookies.get("KEY")
+
+'''
 @app.route("/bodyParams/", methods=['POST', 'GET'])
 def bodyParams():
     body = dict(request.form)
     cookies = dict(request.cookies)
 
     return jsonify({
+        "statusCode": request.headers,
         "method": request.method,
         "body": body,
         "cookies": cookies
     })
 
 
+'''
+Example Redirect
+'''
 @app.route("/redirect/")
 def redirectRoute():
     return redirect("/")
--- src/routes/sampleExternalRoutes.py
+++ src/routes/sampleExternalRoutes.py
@@ -2,7 +2,11 @@ from flask import Blueprint, request, jsonify, session, redirect
 
 SampleRoutes = Blueprint("sample", __name__)
 
+'''
+Example external route
 
+Notice that the decorator is @SampleRoutes and not @app.route
+'''
 @SampleRoutes.route("internalRoute")
 def internalRoute():
     return jsonify({"Internal route": True})
